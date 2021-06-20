da7cd76bfa40229acf21829404dd07dc3c715e26
mhmd nz <muhammad1370@gmail.com>
2021-06-20 19:58:50 +0430

    Remove seeder from Prepration file

--- php/Prepration.sh
+++ php/Prepration.sh
@@ -2,5 +2,4 @@ cd /var/www/html/devolon
 cp .env.example .env
 composer install
 php artisan key:generate
-php artisan migrate:fresh --seed
-php artisan db:seed --class=UserSeeder
+php artisan migrate:fresh
