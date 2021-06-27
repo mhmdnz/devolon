5a9f30f27eb5d5963d41d92aa77422f3eed3db5e
Mohammad Nazari <mohammad.nazari@zoodfood.com>
2021-06-27 12:26:25 +0430

    Add more test case to OfferService unit test

--- tests/Unit/OfferServiceTest.php
+++ tests/Unit/OfferServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Http\DTO\DeleteResultDTO;
+use App\Http\DTO\UpdateResultDTO;
 use App\Models\Offer;
 use App\Models\Product;
 use App\Repositories\Interfaces\OfferRepositoryInterface;
@@ -37,5 +39,61 @@ class OfferServiceTest extends TestCase
         $offerService = new OfferService($offerRepository);
         $result = $offerService->saveOnProduct($product, $offerItems);
         $this->assertEquals($result, $offer);
+    }
+
+    public function test_save(): void
+    {
+        $offer = Offer::factory()->make();
+        $fakeArgs = ['test'];
+        $offerRepository = \Mockery::mock(OfferRepositoryInterface::class)->makePartial();
+        $offerRepository->shouldReceive('save')
+            ->once()
+            ->withArgs([$fakeArgs])
+            ->andReturn($offer);
+        $offerService = new OfferService($offerRepository);
+        $result = $offerService->save($fakeArgs);
+        $this->assertEquals($offer, $result);
+    }
+
+    public function test_update(): void
+    {
+        $offer = Offer::factory()->make();
+        $expectedResult = new UpdateResultDTO(true);
+        $fakeArgs = ['test'];
+        $offerRepository = \Mockery::mock(OfferRepositoryInterface::class)->makePartial();
+        $offerRepository->shouldReceive('update')
+            ->once()
+            ->withArgs([$offer, $fakeArgs])
+            ->andReturnTrue();
+        $offerService = new OfferService($offerRepository);
+        $result = $offerService->update($offer, $fakeArgs);
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function test_delete(): void
+    {
+        $offer = Offer::factory()->make();
+        $expectedResult = new DeleteResultDTO(true);
+        $offerRepository = \Mockery::mock(OfferRepositoryInterface::class)->makePartial();
+        $offerRepository->shouldReceive('delete')
+            ->once()
+            ->withArgs([$offer])
+            ->andReturnTrue();
+        $offerService = new OfferService($offerRepository);
+        $result = $offerService->delete($offer);
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function test_find_or_fail(): void
+    {
+        $offer = Offer::factory()->make();
+        $offerRepository = \Mockery::mock(OfferRepositoryInterface::class)->makePartial();
+        $offerRepository->shouldReceive('findOrFail')
+            ->once()
+            ->withArgs([$offer->id])
+            ->andReturn($offer);
+        $offerService = new OfferService($offerRepository);
+        $result = $offerService->findOrFail($offer->id);
+        $this->assertEquals($offer, $result);
     }
 }
